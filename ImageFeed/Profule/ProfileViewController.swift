0ee75e09c3a9707c6399068eaa46ff53455a4f75
vano3d <vano3d@gmail.com>
2023-08-12 17:46:48 +0300

    Добавил ТабБарКонтроллер

---
+++ ImageFeed/Profule/ProfileViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Ivan Ch on 12.08.2023.
+//
+
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    
+    @IBOutlet var messageTextLabel: UILabel!
+    @IBOutlet var loginTextLabel: UILabel!
+    @IBOutlet var nameTextLabel: UILabel!
+    @IBOutlet var avatarImage: UIImageView!
+    
+    @IBOutlet var logoutButton: UIButton!
+    @IBAction func logoutButtonTapped(_ sender: Any) {
+        
+    }
+}
