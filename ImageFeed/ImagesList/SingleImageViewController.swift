d4b75c3246ab0143d1e450720900ebd8276e80f7
vano3d <vano3d@gmail.com>
2023-08-12 19:26:16 +0300

    Добавлен Navigation Controller

---
+++ ImageFeed/ImagesList/SingleImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Ivan Ch on 12.08.2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+}
+
