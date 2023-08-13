c9e5aec4cf08ad8d8325cf3ce926526424d8af35
vano3d <vano3d@gmail.com>
2023-08-13 23:02:20 +0300

    Доделал согласно учебнику -- удалил Navigation Controller

--- ImageFeed/ImagesList/SingleImageViewController.swift
+++ ImageFeed/ImagesList/SingleImageViewController.swift
@@ -17,6 +17,9 @@ final class SingleImageViewController: UIViewController {
     
     @IBOutlet var imageView: UIImageView!
     
+    @IBAction private func didTapBackButton() {
+          dismiss(animated: true, completion: nil)
+      }
     override func viewDidLoad() {
           super.viewDidLoad()
           imageView.image = image
